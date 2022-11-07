b0857fbd9289cf91aa477d3d69896ca148531698
Vibhavari Bellutagi <vibhavari.bellutagi@gmail.com>
2022-11-07 19:01:44 +0530

    Your branch is up to date with 'origin/feature/searching-algorithms-linear-binary'.  Changes to be committed: 	modified:   Searching/findmaxandmin.py

--- Searching/findmaxandmin.py
+++ Searching/findmaxandmin.py
@@ -1,5 +1,29 @@
+# Introduction: 
+# Problem Statement: find min and max from array 
+# Input: 
+# Output: 
+# Time complexity: 
+
+def find_min(arr):
+    min_num = arr[0]
+    for i in arr:
+        if min_num < i:
+            min_num = i
+    return min_num
+
+def find_max(arr):
+    max_num = arr[0]
+    for i in arr:
+        if max_num > i:
+            max_num = i
+    return max_num
+
 def main():
     arr = [18,12,-7,3,14,28]
+
+    print("Min number in the element - {}".format(find_min(arr)))
+    print("Max number in the element - {}".format(find_max(arr)))
+
 
 
 if __name__ == "__main__":
